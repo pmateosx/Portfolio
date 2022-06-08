44a3ca696fd24bbe6686264e8ed4994e0d056c28
Pablo Mateos <pmateos805@gmail.com>
2022-06-08 23:08:14 +0200

    save

---
+++ components/Navbar.js
@@ -0,0 +1,17 @@
+import { css } from '@emotion/css'
+
+const Navbar = () => {
+    
+    return (
+        <nav>
+            <p>pmattx_</p>
+            <ul>
+                <li>Home</li>
+                <li>Works</li>
+                <li>About</li>
+            </ul>
+        </nav>
+    )
+}
+
+export default Navbar
